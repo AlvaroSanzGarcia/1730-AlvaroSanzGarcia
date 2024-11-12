af98b3920e12e9f437525db9d0dcb721d6d168e8
Alvaro Sanz Garcia <fe7575qi@go.minnstate.edu>
2024-11-11 19:28:41 -0600

    EX3B Initial Setup

---
+++ ex3b-student-web-app/Pages/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace ex3b_student_web_app.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
